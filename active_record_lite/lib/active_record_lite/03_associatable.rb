8d67413e5a099e30694b38352ba6e3474e609e2e
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2014-08-22 21:14:14 -0700

    associations almost done

--- active_record_lite/lib/active_record_lite/03_associatable.rb
+++ active_record_lite/lib/active_record_lite/03_associatable.rb
@@ -10,25 +10,46 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.capitalize.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
-  def initialize(name, options = {})
+  def initialize(name, options = {})  
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+    @class_name = options[:class_name]
+
+    @class_name ||= name.singularize.capitalize
+    @primary_key ||= :id
+    @foreign_key ||= ("#{@class_name.to_s.downcase.singularize}_id").to_sym
+    @name = name
+  end
+end
+
+class HasManyOptions < AssocOptions
+  def initialize(name, self_class_name, options = {})
+    @name = name
+    @self_class_name = self_class_name
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+    @class_name = options[:class_name]
+
+    @class_name ||= name.singularize.capitalize
+    #@self_class_name ||= self
+    @primary_key ||= :id
+    @foreign_key ||= ("#{@self_class_name.to_s.downcase.singularize}_id").to_sym
+  end
+end
+
+module Associatable
+  # Phase IVb
+  def belongs_to(name, options = {})
     define_method(name) do
-      @foreign_key = options[:foreign_key]
-      @primary_key = options[:primary_key]
-      @class_name = options[:class_name]
-
-      @primary_key ||= :id
-      @foreign_key ||= (@class_name.to_s.downcase.singularize + "_id").to_sym
-
-
       query = <<-SQL
       SELECT
       *
@@ -43,22 +64,22 @@ class BelongsToOptions < AssocOptions
       DBconnection.execute(query, value)
     end
   end
-end
-
-class HasManyOptions < AssocOptions
-  def initialize(name, self_class_name, options = {})
-    
-  end
-end
-
-module Associatable
-  # Phase IVb
-  def belongs_to(name, options = {})
-    # ...
-  end
 
   def has_many(name, options = {})
-    # ...
+    define_method(name) do
+      query = <<-SQL
+      SELECT
+      *
+      FROM
+      #{@self_class_name.table_name}
+      WHERE
+      #{@foreign_key} = ?
+      SQL
+
+      value = send(@primary_key)
+
+      DBconnection.execute(query, value)
+    end
   end
 
   def assoc_options
